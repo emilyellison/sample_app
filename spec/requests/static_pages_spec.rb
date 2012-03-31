4c39f77513c38dbfa798ac70307fb38093b57cbe
Emily Ellison <emilyellison986@gmail.com>
2012-03-31 17:56:42 -0500

    Finish Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home Page" do
+    it "should have the heading 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+        :text => 'Ruby on Rails Tutorial Sample App | Home')
+    end
+  end
+  
+  describe "Help Page" do
+    it "should have the heading 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+        :text => 'Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+  
+  describe "About Page" do
+    it "should have the heading 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+        :text => 'Ruby on Rails Tutorial Sample App | About Us')
+    end
+  end
+end
